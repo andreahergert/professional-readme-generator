97afd883089e2ad4c419481abe1d896dd80cd333
Andrea Hergert <ahergert24@hotmail.com>
2022-10-15 09:31:10 -0700

    added markup and corrected list

--- Develop/index.js
+++ Develop/index.js
@@ -1,104 +1,80 @@
-// TODO: Include packages needed for this application
+// Included packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// const generateReadme = ({ name, location, github, linkedin }) =>
-//   `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-//   <title>Document</title>
-// </head>
-// <body>
-//   <div class="jumbotron jumbotron-fluid">
-//   <div class="container">
-//     <h1 class="display-4">Hi! My name is ${name}</h1>
-//     <p class="lead">I am from ${location}.</p>
-//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-//     <ul class="list-group">
-//       <li class="list-group-item">My GitHub username is ${github}</li>
-//       <li class="list-group-item">LinkedIn: ${linkedin}</li>
-//     </ul>
-//   </div>
-// </div>
-// </body>
-// </html>`;
-
-
+// Questions
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
+      name: 'title',
       message: 'What is project title?',
     },
     {
       type: 'input',
-      name: 'location',
+      name: 'description',
       message: 'What is the description of your project?',
     },
     {
       type: 'input',
-      name: 'hobby',
+      name: 'installation',
       message: 'What are your installation instructions for your project?',
     },
     {
       type: 'input',
-      name: 'food',
+      name: 'usage',
       message: 'What is the usage information for your project?',
     },
     {
       type: 'input',
-      name: 'github',
+      name: 'contribution',
       message: 'What are the contribution guidelines for your project?',
     },
     {
       type: 'input',
-      name: 'linkedin',
+      name: 'testing',
       message: 'What are the test instructions for your project?',
     },
-    // Fix list
     {
         type: 'list',
         name: 'license',
         message: 'What license would you like to use for your project?',
-        options: 'None, Apache License 2.0, GNU General Public License v3.0, MIT License'
+        choices: ["MIT", "ISC", "Apache", "GPL", "BSD", "None"],
     },
     {
         type: 'input',
-        name: 'questions',
+        name: 'github',
         message: 'What is your github username?',
     },
       {
         type: 'input',
-        name: 'questions',
+        name: 'github',
         message: 'What is the GitHub repository url for your project?',
       },
       {
         type: 'input',
-        name: 'questions',
+        name: 'email',
         message: 'What is your email address?',
       },
   ])
   .then((answers) => {
-    const readmePageContent = generateReadme(answers);
+    const readmePageContent = generateMarkdown(answers);
 
-    fs.writeFile('readme.md', readmePageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created readme.md!')
+// function to write README file
+    fs.writeFile('README.md', readmePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
     );
   });
 
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
 // // TODO: Create a function to initialize app
-// function init() {}
+// function init() {
+//   inquirer.prompt(questions)
+//   .then(function (userInput) {
+//       console.log(userInput)
+//       writeToFile("README.md", generateMarkdown(userInput));
+//   });
+// };
 
 // // Function call to initialize app
-// init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,51 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+    ## Licensing:
+      [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
+
+    ## Table of Contents 
+      - [Description](#description)
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [Contribution](#contribution)
+      - [Testing](#testing)
+      - [Additional Info](#additional-info)
+
+    ## Description:
+      ${data.description}
+
+    ## Installation:
+      ${data.installation}
+
+    ## Usage:
+      ${data.usage}
+
+    ## License:
+      ${data.licensing}
+
+    ## Contribution:
+      ${data.contribution}
+
+    ## Testing:
+      ${data.testing}
+
+    ## Additional Info:
+      - Github: [${data.github}](https://github.com/${data.github})
+      - Email: ${data.email} `;
 }
 
 module.exports = generateMarkdown;
